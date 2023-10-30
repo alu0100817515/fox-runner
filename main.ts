435d58a8a5273bee335f8fb5a603955d5fed563f
PabloFernandez <145680527+alu0100817515@users.noreply.github.com>
2023-10-30 19:53:16 +0000

    añadi una idle animation cuando se libera la tecla A

--- main.ts
+++ main.ts
@@ -4,19 +4,28 @@ enum ActionKind {
     Jumping
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    for (let index = 0; index < 8; index++) {
-        custom.mover(10, 0)
-    }
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Jumping`,
+    500,
+    false
+    )
     custom.mover(0, 10)
-    custom.mover(0, 10)
-    for (let index = 0; index < 2; index++) {
-        custom.mover(10, 0)
-    }
+    custom.mover(10, 0)
 })
-let mySprite = sprites.create(assets.image`snow_fox`, SpriteKind.Player)
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Idle`,
+    100,
+    true
+    )
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`snow_fox`, SpriteKind.Player)
 scene.setBackgroundImage(assets.image`background`)
 mySprite.setStayInScreen(true)
-mySprite.ay = 5
+mySprite.ay = 50
 tiles.setCurrentTilemap(tilemap`nivel1`)
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 13))
